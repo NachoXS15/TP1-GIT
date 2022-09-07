d07bf84254cf660cfce388117e3a172693393135
NachoXS15 <86374911+NachoXS15@users.noreply.github.com>
2022-09-07 12:56:34 -0300

    Primer ejercicio resuelto

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const countries = require('./countries.json');
  * @returns {Object}
  */
 function getCountryByIso3(isoCode){
-    //Complete with your code
+    const iso3Coded = countries.find (element => element.iso3 == isoCode);
+    return iso3Coded;
 }
 
 
